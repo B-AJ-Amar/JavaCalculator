fcd3b3b40f9000a44a804326644384aa7b66c3dc
B-AJ-Amar <amarbouakaz91@gmail.com>
2024-03-13 13:11:11 +0100

    add(calcLogic):mem and Ans

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,10 +3,26 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Calculator {
+    private String Ans;
+    private String Mem;
     public Calculator(){
-
+        this.Ans = "0";
+        this.Mem = "0";
     }
-    private String calculate(String exp) {
+
+    public String getMem() {
+        return Mem;
+    }
+
+    public void setMem(String mem) {
+        Mem = mem;
+    }
+
+    public String getAns() {
+        return Ans;
+    }
+
+    public String calculate(String exp) {
         String line = null;
         String expression = exp.replace("^", "**");
         expression = expression.replace("÷", "/");
@@ -16,12 +32,15 @@ public class Calculator {
             Runtime rt = Runtime.getRuntime();
             String pythonCode =
                     "from math import *;"
-                            + "math_functions = {'sqrt': sqrt, 'acos': " +
-                            "acos, 'asin': asin, 'atan': atan,  'ceil': ceil, 'cos': cos, " +
-                            "'cosh': cosh,  'exp': exp, 'fabs': fabs, 'floor': floor, 'fmod': fmod, " +
-                            "'frexp': frexp, 'hypot': hypot, 'ldexp': ldexp, 'log': log, 'log10': log10, " +
-                            "'modf': modf, 'pow': pow, 'radians': radians, 'sin': sin, 'sinh': sinh, 'tan': tan, " +
+                            + "math_functions = " +
+                            "{'sqrt': sqrt, 'acos': acos, 'asin': asin, 'atan': atan, " +
+                            "'atan2': atan2, 'ceil': ceil, 'cos': cos, 'cosh': cosh, " +
+                            "'degrees': degrees, 'exp': exp, 'fabs': fabs, 'floor': floor, " +
+                            "'fmod': fmod, 'frexp': frexp, 'hypot': hypot, 'ldexp': ldexp, " +
+                            "'log': log, 'log10': log10, 'modf': modf, 'pow': pow, " +
+                            "'radians': radians, 'sin': sin, 'sinh': sinh, 'tan': tan, " +
                             "'tanh': tanh, 'pi': pi, 'e': e};"
+
                             + "result = eval('"+expression+"', math_functions);"
                             + "print(result)";
             System.out.println(pythonCode);
@@ -40,6 +59,10 @@ public class Calculator {
         catch (IOException e) {
             return "ERROR";
         }
-        return (line == null)? "ERROR" : line;
+        if (line != null) {
+            this.Ans = line;
+            return line;
+        }
+        return "ERROR";
     }
 }
